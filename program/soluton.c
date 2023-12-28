f0ec52754cd3c7c6391f4336dcf80223219f0d22
jby2053 <jby2051@naver.com>
2023-12-28 21:48:02 +0900

    2023.12.28 구조체

--- program/soluton.c
+++ program/soluton.c
@@ -1,108 +1,103 @@
 #include <stdio.h>
-#include <stdlib.h> // 메모리 관련 라이브러리
+#include <math.h>
+
+struct player
+{
+	// 구조체 크기의 경우 멤버 변수의 순서에 따라 메모리의 크기가 다르게
+	// 설정될 수 있습니다.
+
+	char blood;     // 1 byte
+	int health;     // 4 byte
+	float attack;   // 4 byte
+	double defense; // 8 byte
+
+
+
+	// 구조체를 선언하기 전에 구조체는 메모리 공간이
+	// 생성되지 않으므로, 구조체 내부에 있는 데이터를
+	// 초기화할 수 없습니다.
+};
 
 int main()
 {
-#pragma region 범용 포인터 
-	// 자료형이 정해지지 않은 상태로 모든 자료형을
-	// 저장할 수 있는 포인터입니다.
+#pragma region 구조체
+	// 여러 개의 변수를 하나의 집합으로 구조화한 다음
+	// 하나의 객체를 생성하는 것입니다.
+	
+	//struct player player = { 'B', 75, 5.5f };
 
-	//int attack = 100;
-	//float health = 100.0f;
+	////player.attack = 10.5f;
+	////player.blood = 'A';
+	////player.health = 100;
 
-
-	//void* vptr = NULL;
-
-	//vptr = &attack;
-
-	//// 범용 포인터는 메모리 주소에 접근해서 값을 변경할 수 없습니다.
-	//printf("vptr이 가르키는 값 : %d\n", *(int *)vptr);
-
-	//vptr = &health;
-
-	//printf("vptr이 가르키는 값 : %f\n", *(float *)vptr);
-    // 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가 가리키는
-	//// 변수의 자료형으로 형 변환해야 합니다.
-
-
-
+	//printf("player.attack의 값 : %f\n", player.attack);
+	//printf("player.blood의 값 : %c\n", player.blood);
+	//printf("player.health의 값 : %d\n", player.health);
 
 #pragma endregion
 
-#pragma region 동적 할당
-	//// 프로그램을 실행하는 중에 필요한 만큼 메모리를 할당
-	//// 하는 작업입니다.
+#pragma region  바이트 패딩
+   // 멤버 변수를 메모리에서 CPU로 읽을 때 한 번에 읽를 수
+   // 있도록, 컴파일러가 레지스터의 블록에 맞추어 바이트를
+   // 패딩해주는 최적화 작업입니다.
 
-	//int * ptr = malloc(sizeof(int));
-	////int * ptr (스택)
-	////malloc(sizeof(int)); (힙)
+	//printf("player 구조체의 크기 : %d\n", sizeof(struct player));
+   
+	// 구조체의 크기는 구조체를 구성하는 멤버 중에서 크기가 가장 큰
+	// 자료형의 배수가 되도록 정력합니다. 
 
+#pragma endregion
 
-	//*ptr = 100;
-	//// 메모리를 동적 할당할 때 주소를 범용 포인터로
-	//// 반환하기 때문에 자료형을 변환한 다음 메모리에
-	//// 할당해야 합니다.
+#pragma region 두 점 사이의 거리
 
+	struct character
+	{
+		int x;
+		int y;
+	};
 
+	struct Enemy
+	{
+		int x;
+		int y;
+	};
+	
 
-	//printf("*ptr이 가리키는 값 %d\n", *ptr);
+		struct character;   
+		struct Enemy;    
 
-	//free(ptr); 
-	//// 동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로
-	//// 사용이 끝나면 직접 해제 해주어야 합니다.
+		
+		character.x = 0;
+		p1.y = 0;
 
+		
+		p2.x = 6;
+		p2.y = 5;
 
-	//// ptr [] -------> [999] [333] [] [] []
-	//ptr = malloc(sizeof(int) * 5);
+		printf("p1: %d %d\n", p1.x, p1.y);    
+		printf("p2: %d %d\n", p2.x, p2.y);    
 
-	//// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를
-	//// 변경 시킬 수 있으며, 동적으로 메모리의 크기를 할당할
-	//// 때 바이트 단위를 지정합니다.
-
-	//ptr[0] = 999;
-	//ptr[1] = 333;
-
-	//printf("*ptr[0]이 가리키는 값 %d\n", ptr[0]);
-	//printf("*ptr[0]이 가리키는 값 %d\n", ptr[1]);
-
-	//// free(ptr);
-	//// 메모리를 동적으로 할당한 상태에서 해제하지 않으면
-	//// 메모리 누수 현상이 일어나며, 메모리 누수가 일어나게 되면
-	//// 더 이상 메모리를 할당할 수 없는 상태가 됩니다.
+		
 
 	
 
+
+
+
+
+
+	
+
+
+
+	printf("pow 2에 3승 : %lf\n", pow(2,3));
+	printf("Root 25 : %lf\n", sqrt(25));
+
 #pragma endregion
-
-#pragma region 단축 평가 계산
-	// 논리 연산에 두 파연산자 중 왼쪽에 있는 결과가
-	// "거짓"이면 우측의 피연산자의 값은 평가하지 않고
-	// 바로 결과를 얻어내는 계산입니다.
-
-	    // true && true // true (엔드)
-		// true && false // false (엔드)
-		// true || false // true (오알)
- 		// false || true // true (오알)
-
-	//int x = 0;
-	//int y = 0;
-
-	//if (x < y || ++x)
-	//{
-	//	printf("short curcuit\n");
-	//}
-	//	printf("x 변수의 값 : %d\n", x);
-
-	//if (x < y || y++)
-	//{
-	//	printf("short curcuit\n");
-	//}
-	//	printf("y 변수의 값 : %d\n", y);
 
 
 #pragma endregion
+
 
 	return 0;
-
-
 }
